d1a5b9574b524b003bbcba834c793ebeeccda34e
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2023-01-16 14:06:59 +0200

    Pythonin perusteet

---
+++ Kokeilu.py
@@ -0,0 +1,21 @@
+print("Hei!")
+print("hei")
+
+for ihminen in ["Keijo", "Kiia", "Vieno", "Sandro"]:
+    print("Hei", ihminen)
+    print("nimessäni on", len(ihminen), "kirjainta")
+
+luku = 42
+print("Luku on", luku)
+
+# Merkkijonot voidaan kirjoittaa lainausmerkeillä("") tai heittomerkeillä('')
+merkkijono = "Punainen"
+merkkijono2 = 'Sininen'
+
+print("Merkkijonot ovat:", merkkijono, "ja", merkkijono2)
+
+lista = ["Merkkijono",
+        [1, 2, 3]
+]
+print(lista)
+
