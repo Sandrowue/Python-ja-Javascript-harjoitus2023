53e41fa9f6c3ba7b27631dd0a586ec5a73c7bbff
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2023-01-23 11:32:07 +0200

    if for export

---
+++ harjoitus2_funktio.py
@@ -0,0 +1,38 @@
+#Tähän mennessä käytetyjä funktioita
+# print(x)  tulostaa x
+# X = input() luetaan merkkijono muuttujaan x
+# str(X) muutetaan X merkkijonoksi
+
+def kysy_nimi():
+    nimi = input('Anna nimi: ')
+    return nimi
+joku = kysy_nimi()
+print(joku)
+
+def kysy_nimet(lkm):
+    nimet = []
+    while len(nimet) < lkm:
+       nimi = kysy_nimi()
+       nimet.append(nimi)
+    return nimet
+
+
+if __name__ == "__main__":
+    nimilista = kysy_nimet(3)
+    print(nimilista)
+
+
+
+'''def kysy_nimet_rekuriolla(lkm, nimet=None):
+    if jeljellä == 0:
+        return nimet
+    if nimet is None:
+        nimet = []
+    nimi = kysy_nimi()
+    return kysy_nimet_rekursiolla(jäkjellä - 1, nimet + [nimi])
+    
+nimet = kysy_nimet_rekursiolla(3)
+print(nimet)'''
+
+
+
---
+++ harjotus2b.py
@@ -0,0 +1,6 @@
+import harjoitus2_funktio
+
+
+print("Kysytään 2 niemä:")
+nimet = harjoitus2_funktio.kysy_nimet(2) 
+print("Nimet ovat", nimet)
