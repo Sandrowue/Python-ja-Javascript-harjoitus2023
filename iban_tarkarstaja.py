7ec44c1ea492f90283116c8fa2a7c8744acfef66
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2023-01-30 13:28:33 +0200

    valmis

--- iban_tarkarstaja.py
+++ iban_tarkarstaja.py
@@ -1,17 +1,17 @@
 from stdnum import iban
 
-def main():
-    pass
+
 def main():
     tilinumero = input('Anna IBAN-tilinumero: ')
     on_oikea_iban = iban.is_valid(tilinumero)
     if on_oikea_iban:
         print(f"Antamasi tilinumero {tilinumero} on oikea IBAN")
+        muotoilu = iban.format(tilinumero)
+        print(f'Tilinumero muotoiltuna: {muotoilu}')
     else:
         print(f"Antamasi tilinumer {tilinumero} ei ole oikea IBAN")
 
 
-if __name__ == main:
+if __name__ == '__main__':
     main()
 
-#requirements
